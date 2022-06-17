efa3303ea1dce162ca05396a00da934e8584d67e
Tang <Tang@atzhuoyue.com>
2022-06-17 10:31:18 +0800

    first commit

---
+++ src/main/java/com/atzhuoyue/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atzhuoyue.git;
+
+/**
+ * @auther tzystart
+ * @creat 2022-06-17 10:26
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+
+}
