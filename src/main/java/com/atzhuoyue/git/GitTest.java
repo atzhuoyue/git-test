27b09f114ca014811969d85d0f755040280b8a6e
Tang <Tang@atzhuoyue.com>
2022-11-20 16:58:18 +0800

    idea git commit first

---
+++ src/main/java/com/atzhuoyue/git/GitTest.java
@@ -0,0 +1,13 @@
+package com.atzhuoyue.git;
+
+/**
+ * @auther tzystart
+ * @creat 2022-11-20 16:55
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("idea git first test");
+    }
+
+}
